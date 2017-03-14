37bbe39ccc7a23cafcf6c43cc83d0998e6b47f09
Patrick Nolan <patnolan33@gmail.com>
2017-03-14 10:24:52 -0500

    User Story ID: 7; Task ID: 12. Implemented a user input to illustrate how the human would be in the loop on the decision making process of the robot. For each frame the robot takes, it will suggest an action. The Human can then decide to continue or to stop the robot motion. Ideally, this would be used fro safety purposes.

--- app/main.cpp
+++ app/main.cpp
@@ -10,7 +10,15 @@ int main()
 {
 	std::shared_ptr<VisionSystem> visionSystem = std::make_shared<VisionSystem>(10, 0);
 
-	std::cout << "Suggested action: " << visionSystem->determineControlAction() << std::endl;
+	char userInput;
+	while (userInput != 'n' && userInput != 'N') {
+		std::cout << std::endl << "Suggested action: " << visionSystem->determineControlAction() << std::endl;
+
+		std::cout << "Would you like the robot to continue? (y/n)" << std::endl;
+		std::cin >> userInput;
+	}
+
+	std::cout << "User terminated motion. Stopping robot." << std::endl;
 
     return 0;
 }
